2bb3e24db94cc4aca085f171050f934bf06804a2
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-10-24 16:34:14 +0000

    Added payment results

--- app.rb
+++ app.rb
@@ -33,5 +33,14 @@ get("/square/new") do
   @apr = params.fetch("apr").to_f
   @years = params.fetch("years").to_f
   @principal = params.fetch("principal").to_f
+
+  @number_of_periods = @years * 12
+  @apr_percentage = @apr / 100
+  @rate_per_period = @apr_percentage / 12
+
+  @monthly_payment_numerator = @rate_per_period * @principal
+  @monthly_payment_denominator = 1 - (1 + @rate_per_period) ** (-1 * @number_of_periods)
+
+  @payment_result = @monthly_payment_numerator / @monthly_payment_denominator
   erb(:payment_results)
  end
