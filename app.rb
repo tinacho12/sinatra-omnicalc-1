3aa345fc261de899b2fa749be219d77b07ea2c6b
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-10-24 16:10:35 +0000

    Added square root form

--- app.rb
+++ app.rb
@@ -14,3 +14,13 @@ get("/square/new") do
   @the_result = @the_num ** 2
   erb(:square_results_simplified)
  end
+
+ get("/square_root/new") do
+  erb(:square_root)
+ end
+
+ get("/square_root/results") do
+  @the_num = params.fetch("number").to_f
+  @the_result = Math.sqrt(@the_num)
+  erb(:square_root_results)
+ end
