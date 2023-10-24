0e27017c08734c340c2609829b93ce1daddce9f2
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-10-24 03:19:09 +0000

    Starting from scratch

--- app.rb
+++ app.rb
@@ -2,17 +2,5 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  erb(:homepage)
+  erb(:square)
 end
-
-=begin
-If I visit the path /square/new, I should see a form with a label and an input to enter a number.
-If I submit that form, I should see the square of the number that I entered.
-
-If I visit the path /square_root/new, I should see a form with a label and an input to enter a number.
-If I submit that form, I should see the square root of the number that I entered.
-
-If I visit the path /payment/new, I should see a form with labels and inputs to enter three values:
-
-If I visit the path /random/new, I should see a form with labels and inputs to enter two numbers, a minimum and a maximum.
-=end
