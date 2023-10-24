3b9c8c9ac14e67411897c2ed16eb3cf942feb70d
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-10-24 02:12:03 +0000

    Added homepage content

--- app.rb
+++ app.rb
@@ -2,8 +2,17 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
 end
+
+=begin
+If I visit the path /square/new, I should see a form with a label and an input to enter a number.
+If I submit that form, I should see the square of the number that I entered.
+
+If I visit the path /square_root/new, I should see a form with a label and an input to enter a number.
+If I submit that form, I should see the square root of the number that I entered.
+
+If I visit the path /payment/new, I should see a form with labels and inputs to enter three values:
+
+If I visit the path /random/new, I should see a form with labels and inputs to enter two numbers, a minimum and a maximum.
+=end
